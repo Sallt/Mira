0211110f703ca24fb100f2557c197adc290f8f67
Sallt <44704718+Sallt@users.noreply.github.com>
2022-06-08 09:49:12 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,25 +1,38 @@
 import os
+from tkinter import Button
+
+from kivy.graphics import Color, Rectangle
 from kivy.lang import Builder
 from kivymd.app import MDApp
+from kivymd.toast import toast
 
+from kivy.uix.modalview import ModalView
+from kivymd.uix.filemanager import MDFileManager
 from kivy.uix.screenmanager import ScreenManager, Screen
-from kivy.uix.screenmanager import RiseInTransition
 from kivymd.uix.navigationdrawer import MDNavigationDrawer
-from kivymd.uix.carousel import MDCarousel
 from kivy.properties import ObjectProperty, StringProperty, BooleanProperty, ListProperty, get_color_from_hex
 from kivy.core.window import Window
+from kivy.uix.scrollview import ScrollView
+from kivy.uix.image import Image
 from kivy.clock import Clock
 # from kivymd.uix.screen import MDScreen
 # from akivymd import *
-from kivy.uix.widget import Widget
+# from kivy.uix.widget import Widget
 # from libs import programdata as data
 # from libs.uix.dialogue import dialog
+from kivymd.uix.button import MDRectangleFlatButton, MDRectangleFlatIconButton, MDFloatingActionButton
+from kivy.uix.anchorlayout import AnchorLayout
+from kivy.graphics.instructions import Canvas
+# from kivy.core.image import Image
+from kivymd.uix.menu import MDDropdownMenu
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.boxlayout import BoxLayout
 from kivymd.uix.label import MDLabel
-from kivymd.uix.button import MDRectangleFlatButton, MDRectangleFlatIconButton, MDFloatingActionButton
-from kivy.uix.anchorlayout import AnchorLayout
-from kivy.core.image import Image
+from kivymd.uix.gridlayout import GridLayout
+from PIL import Image as Image1
+
+img_size = Image1.open("data/images/rep_8a.jpg").size
+
 
 class MobileApp(MDApp):
 
@@ -30,31 +43,84 @@ class MobileApp(MDApp):
 
         # self.screen = StartScreen(events_callback=self.events_program)
         self.window = Window
+        self.screen = Builder.load_file('libs/uix/kv/startscreen.kv')
         # self.dialog_load_contact = None
         # self.open_exit_dialog = None
         # self.screen_add_groups = None  # kivy.lang.builder.AddContactAddGroups
         # self.data = data
         # self.current_tab = 'classes'
+        self.manager_open = False
+        self.manager = None
+
+    def callback_for_menu_items(self, *args):
+        toast(args[0])
 
     def build(self):
         self.title = "Опросник Мира"
         self.icon = 'data/images/logo.png'
         self.theme_cls.primary_palette = "Teal"
         self.theme_cls.material_style = "M2"
-        self.window.size = (400, 740)
+        self.window.size = (360, 740)
+        self.items_class = [
+            {
+                "viewclass": "MDMenuItem",
+                "text": f'{i}',
+                "callback": self.callback_for_menu_items(i),
+            } for i in ["6 Б", "8 A"]]
+        self.items_set = [
+            {
+                "viewclass": "MDMenuItem",
+                "text": "Тригонометрия",
+                "callback": self.callback_for_menu_items,
+            }]
         # return self.load_kv_files()
         # Builder.load_file('libs/uix/kv/startscreen.kv')
         # self.screen = StartScreen()
         # self.manager_tab_classes = self.screen.ids.sm_tab_classes
 
-        kv = Builder.load_file('libs/uix/kv/startscreen.kv')
+        # kv = Builder.load_file('libs/uix/kv/startscreen.kv')
         # return Builder.load_file('/Users/olga/PycharmProjects/Mira/libs/uix/kv/startscreen.kv')
         # return kv
 
         # sm = Manager()
         # sm.add_widget(Screen1(name='empty'))
         # sm.add_widget(Screen2(name='cr_class'))
-        return kv
+        return self.screen
+
+    def file_manager_open(self):
+        if not self.manager:
+            self.manager = ModalView(size_hint=(1, 1), auto_dismiss=False)
+            self.file_manager = MDFileManager(
+                exit_manager=self.exit_manager, select_path=self.select_path)
+            self.manager.add_widget(self.file_manager)
+            self.file_manager.show('/')  # output manager to the screen
+        self.manager_open = True
+        self.manager.open()
+
+    def select_path(self, path):
+        '''It will be called when you click on the file name
+        or the catalog selection button.
+
+        :type path: str;
+        :param path: path to the selected directory or file;
+        '''
+
+        self.exit_manager()
+        toast(path)
+
+    def exit_manager(self, *args):
+        '''Called when the user reaches the root of the directory tree.'''
+
+        self.manager.dismiss()
+        self.manager_open = False
+
+    def events(self, instance, keyboard, keycode, text, modifiers):
+        '''Called when buttons are pressed on the mobile device..'''
+
+        if keyboard in (1001, 27):
+            if self.manager_open:
+                self.file_manager.back()
+        return True
 
     def change_screen(self, scr_name):
         self.root.current = scr_name
@@ -199,7 +265,29 @@ class ScoresheetScreen(Screen):
 
 
 class QuizScreen(Screen):
-    pass
+    # def __init__(self):
+    #     menu_items = [
+    #         {
+    #             "text": f"Item {i}",
+    #             "viewclass": "Item",
+    #             "height": 54,
+    #             "on_release": lambda x=f"Item {i}": self.menu_callback(x),
+    #         } for i in range(5)
+    #     ]
+    #     self.class_menu = MDDropdownMenu(
+    #         caller=self.screen.ids.button,
+    #         items=menu_items,
+    #         width_mult=4,
+    #     )
+    #     self.set_menu = MDDropdownMenu(
+    #         caller=self.screen.ids.button,
+    #         items=menu_items,
+    #         width_mult=4,
+    #     )
+
+    def menu_callback(self, text_item):
+        print(text_item)
+        self.add_widget(self.class_menu)
 
 
 class EmptyScreen(FloatLayout):
@@ -207,6 +295,33 @@ class EmptyScreen(FloatLayout):
     text = StringProperty()
     callback = ObjectProperty()
     disabled = BooleanProperty()
+
+
+class ShowList(FloatLayout):
+    callback = ObjectProperty()
+    disabled = BooleanProperty()
+
+
+class MyImage(Image):
+    def get_size_for_notebook(self, **kwargs):
+        global img_size
+        width, height = (360, 740)
+        return 2*width, (img_size[0] * height / width)
+
+
+class MyScrollView(ScrollView):
+    def on_scroll_y(self, instance, scroll_val):
+        if scroll_val < 0.05:  # no logic for this number
+            box = MDApp.get_running_app().root.ids.notebook_images
+            new_image = MyImage()
+            box.add_widget(new_image)
+            self.scroll_y = new_image.height / box.height
+
+
+class Report(GridLayout):
+    def __init__(self, **kwargs):
+        self.rows = 1
+        super(Report, self).__init__(**kwargs)
 
 
 class CreateClass(Screen):
@@ -246,9 +361,6 @@ class CreateSet(Screen):
 
 
 class QuizCarousel(BoxLayout):
-    # def __init__(self):
-    #     self.quiz_canva = self.children
-
     cl_name = StringProperty()
     set_n = StringProperty()
     image = StringProperty()
@@ -260,63 +372,85 @@ class QuizCarousel(BoxLayout):
     answer_d = StringProperty()
     quiz_canva = ListProperty()
     quest_count = None
-
-    # def set_canvas(self):
-    #     set_name = MDRectangleFlatButton(pos_hint={'center_x': .5, 'center_y': 0}, id='set_name',
-    #                                      md_bg_color_disabled=get_color_from_hex("#4bb760"),
-    #                                      disabled_color=[1, 1, 1, 1], text= self.set_n, theme_text_color="Custom",
-    #                                      text_color=[1, 1, 1, 1], disabled=True)
-    #     img = Image(size_hint=(1, .25), source=self.image, opacity=.3)
-    #     q_txt = MDLabel(size_hint=(1, .15), id="question", font_style='H6', theme_text_color="Custom",
-    #                     text_color=get_color_from_hex("#42636b"), text=self.q_text, halign='center')
-    #     box = BoxLayout(id='answers', orientation='vertical', size_hint=(.9, .4), pos_hint={'center_x': .5})
-    #     answer = MDRectangleFlatIconButton(size_hint=(1, .25), line_width=1.5, icon_color=[0, .5, 0, 1])
-    #     box.add_widget(answer(icon="data/images/icon_a.png", text=self.answer_a))
-    #     box.add_widget(answer(icon="data/images/icon_b.png", text=self.answer_b))
-    #     box.add_widget(answer(icon="data/images/icon_c.png", text=self.answer_c))
-    #     box.add_widget(answer(icon="data/images/icon_d.png", text=self.answer_d))
-    #
-    #     anch = AnchorLayout(size_hint=(1, None), height=60)
-    #     cam = MDFloatingActionButton(id='cam_button', icon='camera-enhance', size_hint=(None, None),
-    #                                  size=(56, 56), opposite_colors=True, elevation_normal=8,
-    #                                  pos_hint={'center_x': .5, 'center_y': 0}, on_release=self.scan_class())
-    #     anch.add_widget(cam)
-    #     self.quiz_canva = [set_name, img, q_txt, box, anch]
+    was_run = None
 
     def parse_set(self):
-        self.set_n = "Тригонометрия как она есть"
-        self.image = "data/images/tumbleweed.png"
+        if self.was_run == 1:
+            self.clear_widgets()
+            self.quiz_canva.reverse()
+            for ch in self.quiz_canva:
+                self.add_widget(ch)
+        self.set_n = "Стереометрия"
+        self.image = "data/images/tetraedr.png"
         self.q_text = "Сколько граней у тетраедра?"
-        self.answer_a = "-одна-"
-        self.answer_b = "-шесть-"
-        self.answer_c = "-четыре-"
-        self.answer_d = "-три-"
-        self.quest_count = 2
+        self.answer_a = " две"
+        self.answer_b = " три"
+        self.answer_c = " четыре"
+        self.answer_d = " пять"
+        self.quest_count = 8
 
     def scan_class(self):
         # считывание данных
         # + сохранение данных, считанных камерой
-
-        # if self.children == []:
-        #     self.children = self.quiz_canva
         self.next_question()
 
     def next_question(self):
         self.quest_count = self.quest_count - 1
+        self.quiz_canva = []
 
         if self.quest_count == 0:
+            self.quiz_canva = self.children
+            self.was_run = 1
             self.clear_widgets()
             box = BoxLayout()
             box.add_widget(MDLabel(text='Вы завершили прохождение теста!', halign='center'))
             self.add_widget(box)
         else:
             self.quiz_canva = self.children
-            self.image = "data/images/brush.png"
-            self.q_text = "Смесь каких цветов даёт пурпурный оттенок?"
-            self.answer_a = "-синий, красный-"
-            self.answer_b = "-желтый, зеленый-"
-            self.answer_c = "-оранжевый, голубой, розовый-"
-            self.answer_d = "-красный, белый, голубой-"
+            if self.quest_count > 4:
+                self.q_text = "Сколько у тетраедра диагоналей?"
+                self.answer_a = " 3"
+                self.answer_b = " 4"
+                self.answer_c = " 6"
+                self.answer_d = " не имеет"
+                if self.quest_count == 7:
+                    self.q_text = "Сколько оснований имеет тетраедр?"
+                    self.answer_a = " 1"
+                    self.answer_b = " 2"
+                    self.answer_c = "3"
+                    self.answer_d = " 4"
+                if self.quest_count == 6:
+                    self.q_text = "Что лежит в основании тетраедра?"
+                    self.answer_a = " четрехугольник"
+                    self.answer_b = " пятиугольник"
+                    self.answer_c = " треугольник"
+                    self.answer_d = " шестиугольник"
+            elif self.quest_count > 0:
+                self.image = "data/images/tetr.png"
+                self.q_text = "Сколько диагоналей у параллелепипеда?"
+                self.answer_a = " 3"
+                self.answer_b = " 4"
+                self.answer_c = " 6"
+                self.answer_d = " не имеет"
+                if self.quest_count == 4:
+                    self.q_text = "Сколько вершин имеет параллелепипед?"
+                    self.answer_a = " 4"
+                    self.answer_b = " 6"
+                    self.answer_c = " 8"
+                    self.answer_d = " 12"
+                if self.quest_count == 3:
+                    self.q_text = "Сколько оснований имеет параллелепипед?"
+                    self.answer_a = " 1"
+                    self.answer_b = " 2"
+                    self.answer_c = " 3"
+                    self.answer_d = " 4"
+                if self.quest_count == 2:
+                    self.q_text = "Из каких геометрических фигур состоит параллелепипед?"
+                    self.answer_a = " параллелограммов"
+                    self.answer_b = " треугольников"
+                    self.answer_c = " пятиугольников"
+                    self.answer_d = " трапеций"
+
             self.clear_widgets()
             self.quiz_canva.reverse()
             for ch in self.quiz_canva:
